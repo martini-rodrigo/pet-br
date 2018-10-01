75a2a57018528beb7285d57282e11b11276d3c07
rmartini <rodrigocostamartini@gmail.com>
2018-10-01 14:01:49 -0300

    SPRING INITIALIZR

---
+++ src/main/java/br/com/pet/PetBrApplication.java
@@ -0,0 +1,12 @@
+package br.com.pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetBrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetBrApplication.class, args);
+	}
+}
