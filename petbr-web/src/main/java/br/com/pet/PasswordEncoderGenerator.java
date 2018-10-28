56e213d797a168c4132f64eb1f0e31becc173b17
martini-rodrigo <rodrigocostamartini@gmail.com>
2018-10-28 12:05:09 -0300

    teste 2

--- petbr-web/src/main/java/br/com/pet/PasswordEncoderGenerator.java
+++ petbr-web/src/main/java/br/com/pet/PasswordEncoderGenerator.java
@@ -11,7 +11,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class PasswordEncoderGenerator {
 
     public static void main(String[] args) {
-    	
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         System.out.println(passwordEncoder.encode("nimda"));
         System.out.println(passwordEncoder.matches("nimda", "$2a$10$x9Xo8eAvtOahZ1UGJJT0Fu8SByGZrgvgM4ssf59pxsoLPjtvMwmb."));
